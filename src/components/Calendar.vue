7261d7533e3a8fa1990543c3099aa78934ebd36a
Nishant <nishant301294@gmail.com>
2017-03-30 12:27:54 -0400

    Add calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="container">
+    <h1>Calendar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  #container {
+    flex: 1;
+    overflow: auto;
+  }
+</style>
