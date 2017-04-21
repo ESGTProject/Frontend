d31ed1b720d8f449b9387481beb47df68b87ccf0
Nishant <nishant301294@gmail.com>
2017-04-21 15:03:49 -0400

    Finalized clock, removing analog clock

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,10 +1,10 @@
 <template lang="html">
   <div id="container">
-    <h1>Clock</h1>
+    <!-- <h1>Clock</h1> -->
     <h2>{{date}}</h2>
     <h2>{{time}}</h2>
     </br>
-    <iframe src="http://free.timeanddate.com/clock/i5ntccrp/n25/szw110/szh110/hbw0/hfc000/cf100/hgr0/fav0/fiv0/mqcfff/mql15/mqw4/mqd94/mhcfff/mhl15/mhw4/mhd94/mmv0/hhcbbb/hmcddd/hsceee" frameborder="0" width="110" height="110"></iframe>
+    <!-- <iframe src="http://free.timeanddate.com/clock/i5ntccrp/n25/szw110/szh110/hbw0/hfc000/cf100/hgr0/fav0/fiv0/mqcfff/mql15/mqw4/mqd94/mhcfff/mhl15/mhw4/mhd94/mmv0/hhcbbb/hmcddd/hsceee" frameborder="0" width="110" height="110"></iframe> -->
   </div>
 </template>
 
@@ -29,8 +29,16 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
 #container {
-  flex: 0 auto;
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+}
+h2 {
+  margin: 2px;
+  font-size: 2.2em;
+  align-self: flex-start;
 }
 </style>
